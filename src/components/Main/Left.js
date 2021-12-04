7370813fccf7a4a567bdeb15fb87e0c55de55ae3
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2021-12-04 19:53:26 -0300

    bonus: video

--- src/components/Main/Left.js
+++ src/components/Main/Left.js
@@ -35,45 +35,65 @@ function Stories(){
 function Posts(){
     return(
         <div class="posts">
-            {UsersPosts.map( ({img,user,post,likeImg, likeUser}) => {
+            {UsersPosts
+                .map( (post) => {
+                    console.log(post)
                 return (
                     <div class="post">
-                        <div class="topo">
-                            <div class="usuario">
-                                <img src={img} />
-                                {user}
+                        <Topo post={post}/>
+                        {post.type === "img" 
+                        ?   <div class="conteudo">
+                                <img src={post.content}/>
                             </div>
-                            <div class="acoes">
-                                <ion-icon name="ellipsis-horizontal"></ion-icon>
+                        :   <div class="conteudo">
+                                <video width="612" height="612" autoPlay muted>
+                                    <source src={post.contentMp4} type="video/mp4"/>   
+                                    <source src={post.contentOgv} type="video/ogv"/>   
+                                </video>
                             </div>
-                        </div>
-
-                        <div class="conteudo">
-                        <img src={post}/>
-                        </div>
-
-                        <div class="fundo">
-                            <div class="acoes">
-                                <div>
-                                    <ion-icon name="heart-outline"></ion-icon>
-                                    <ion-icon name="chatbubble-outline"></ion-icon>
-                                    <ion-icon name="paper-plane-outline"></ion-icon>
-                                </div>
-                                <div>
-                                    <ion-icon name="bookmark-outline"></ion-icon>
-                                </div>
-                            </div>
-
-                            <div class="curtidas">
-                                <img src={likeImg}/>
-                                <div class="texto">
-                                    Curtido por <strong>{likeUser}</strong> e <strong>outras 101.523 pessoas</strong>
-                                </div>
-                            </div>
-                        </div>
+                        }
+                        <Fundo post={post}/>
                     </div>
                 );
             })}
+        </div>
+    );
+}
+
+function Topo( { post } ) {
+    return(
+        <div class="topo">
+            <div class="usuario">
+                <img src={post.img} />
+                {post.user}
+            </div>
+            <div class="acoes">
+                <ion-icon name="ellipsis-horizontal"></ion-icon>
+            </div>
+        </div>
+    );
+}
+
+function Fundo( { post } ){
+    return(
+        <div class="fundo">
+            <div class="acoes">
+                <div>
+                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon name="chatbubble-outline"></ion-icon>
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                </div>
+                <div>
+                    <ion-icon name="bookmark-outline"></ion-icon>
+                </div>
+            </div>
+
+            <div class="curtidas">
+                <img src={post.likeImg}/>
+                <div class="texto">
+                    Curtido por <strong>{post.likeUser}</strong> e <strong>outras 101.523 pessoas</strong>
+                </div>
+            </div>
         </div>
     );
 }
--- src/data/UsersPosts.js
+++ src/data/UsersPosts.js
@@ -1,25 +1,40 @@
 import imgPost01 from "../assets/img/meowed.svg"
 import imgPost02 from "../assets/img/barked.svg"
+import imgPost03 from "../assets/img/9gag.svg"
 import post01 from "../assets/img/dog.svg"
 import post02 from "../assets/img/gato-telefone.svg"
+import post03mp4 from "../assets/video/video.mp4"
+import post03ogv from "../assets/video/video.ogv"
 import likePost01 from "../assets/img/respondeai.svg"
 import likePost02 from "../assets/img/adorable_animals.svg"
+import likePost03 from "../assets/img/meowed.svg"
 
 export let UsersPosts = [
     {
+        type: "img",
         img: imgPost01,
         user: "meowed",
-        post: post01,
+        content: post01,
         likeImg: likePost01,
         likeUser: "respondeai"
 
     },
     {
+        type: "img",
         img: imgPost02,
         user: "barked",
-        post: post02,
+        content: post02,
         likeImg: likePost02,
         likeUser: "adorable_animals"
+    },
+    {
+        type: "video",
+        img: imgPost03,
+        user: "9gag",
+        contentMp4: post03mp4,
+        contentOgv: post03ogv,
+        likeImg: likePost03,
+        likeUser: "meowed"
     }
 ]
 
